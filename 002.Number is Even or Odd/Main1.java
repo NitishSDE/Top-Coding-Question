c09825e1ea6092f50e113c06fd17518c4f43df03
NitishSDE <nkroy1993@yahoo.com>
2023-12-01 18:38:11 +0530

    002.Number is Even or Odd

--- 002.Number is Even or Odd/Main1.java
+++ 002.Number is Even or Odd/Main1.java
@@ -1,12 +1,11 @@
 import java.util.*;
 public class Main1 {
+    // Function Defining to check Even or Odd.
     public static String checkEvenOdd(int n){
        return (n % 2 == 0) ? "Even" : "Odd";
     }
 
     public static void main(String[] args) {
-        checkEvenOdd(12);
-        checkEvenOdd(45);
         System.out.println("Enter a Number");
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
